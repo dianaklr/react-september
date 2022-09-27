a9c42a7c65461bb6a79deb9f4f6d73874087c91a
dianaklr <diana.klr148@gmail.com>
2022-09-27 20:47:46 +0300

    hw 2 (part 2)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@
 //     потрібно вивести всі запуски кораблів окрім 2020 року
 // репрезентувати тільки окремі поля (зазначені в скрнішоті в папці)
 import './App.css';
+import Launches from "./launches/Launches";
 
 
 
 function App() {
   return (
     <div className="App">
-
+<Launches/>
     </div>
   );
 }
--- src/launch/Launch.js
+++ src/launch/Launch.js
@@ -1,9 +1,15 @@
+import '../launch/Launch.css';
 export default function Launch({launch}){
+    if(launch.launch_year!=2020){
     return(
-        <div>
+
+        <div className={'flight'}>
             <p>{launch.mission_name}</p>
-            <p>{launch.launch}</p>
+            <p>{launch.launch_year}</p>
+            <img className={'flightImg'} src={launch.links.mission_patch_small}/>
+
         </div>
     )
+    }
 }
 
