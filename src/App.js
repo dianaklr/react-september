a8bb030e1baea75198525e16a2677db7a4832ba0
dianaklr <diana.klr148@gmail.com>
2022-10-06 18:50:29 +0300

    form 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+// не контрольований компонент, тому що реакт не контролює стан інпутів
 
 import './App.css';
-import {users} from "./components/userList/list";
-import Users from "./components/users/Users";
+import {createRef} from "react";
 
 function App() {
+
+    let login = createRef();
+    let password = createRef();
+
+    function onSubmit(e){
+        console.log(login.current.value);
+        console.log(password.current.value); //вивести поточне значення інпута
+        // create obj API
+        e.preventDefault(); // для того щоб сторінка не перезавантажувалась
+    }
+
   return (
     <div className="App">
-    <p>list User:</p>
-    <Users/>
+      <form onSubmit={onSubmit}>
+          <input type="text" name={'login'} ref={login}/>
+          <input type="text" name={'password'} ref={password}/>
+        <button>submit</button>
+
+      </form>
 
     </div>
   );
